d01510b2898669de60206f3d262ceeab66778179
EmiS <EmiS@EmiS-Komputer>
2016-01-22 18:17:13 +0100

    Alarm2

---
+++ iHouse/src/iHouse/Alarm.java
@@ -0,0 +1,20 @@
+package iHouse;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alarm {
+
+	CzujnikAlarmu kuchnia = new CzujnikAlarmu(1,"kuchnia");
+	CzujnikAlarmu salon = new CzujnikAlarmu(2,"salon");
+	CzujnikAlarmu pokoj = new CzujnikAlarmu(3,"pokoj");
+	CzujnikAlarmu garaz = new CzujnikAlarmu(4,"garaz");
+	
+	List<String> czujnik = new ArrayList<String>();
+	czujnik.add(kuchnia);
+	
+	
+	
+	
+}
